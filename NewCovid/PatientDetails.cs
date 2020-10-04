b11d215fd2cc6e150fe6962eb00df8f2d74d71ab
rups1234 <joshi08.rupali@gmail.com>
2020-10-04 23:45:38 +0530

    First Commit

---
+++ NewCovid/PatientDetails.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewCovid
+{
+    class PatientDetails
+    {
+
+
+        public Dictionary<int, PatientDetails> ptdata = new Dictionary<int, PatientDetails>();
+
+
+        public int patient_ID;
+        public string patient_Name;
+        public int patient_phone;
+        public int patient_age;
+        public string symptoms;
+        public string covidPt;
+
+
+        //Add method for Setter & Getter
+
+        //public PatientDetails SetPtDetails()
+        //{
+
+        //}
+        public void Setpatient_ID(int patient_ID) => this.patient_ID = patient_ID;
+        public void Setpatient_Name(string patient_Name) => this.patient_Name = patient_Name;
+
+        public void Setpatient_phone(int patient_phone) => this.patient_phone = patient_phone;
+        public void Setpatient_age(int patient_age) => this.patient_age = patient_age;
+
+        public void Setsymptoms(string symptoms) => this.symptoms = symptoms;
+        public void SetcovidPt(string covidPt) => this.covidPt = covidPt;
+
+
+
+        public int Getpatient_ID() => patient_ID;
+        public string Getpatient_Name() => patient_Name;
+        public int Getpatient_phone() => patient_phone;
+        public int Getpatient_age() => patient_age;
+        public string Getsymptoms() => symptoms;
+        public string GetcovidPt() => covidPt;
+
+
+        public void Setpatient_detail(Dictionary<int, PatientDetails> ptdata) => this.ptdata = ptdata;
+
+        public Dictionary<int, PatientDetails> Getpatient_detail() => ptdata;
+
+
+    }
+
+
+}
---
+++ NewCovid/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewCovid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PatientDetails pobject = new PatientDetails();
+
+            Dictionary<int, PatientDetails> newdic = pobject.Getpatient_detail();
+     
+            pobject.Setpatient_detail();
+          
+
+            AddMethod(101,"Jim",768987677,77,"Fever","YES");
+
+        }
+
+
+
+        static PatientDetails AddMethod(int patient_ID, string patient_Name, int patient_phone, int patient_age,  string symptoms, string covidPt)
+        {
+
+            PatientDetails objectptdetails = new PatientDetails();
+
+           
+
+
+            return objectptdetails;
+        
+        
+        
+        }
+
+        public void CovidCheck(Dictionary<int, PatientDetails> newdic)
+        {
+            foreach(var check in newdic)
+            {
+                PatientDetails p = check.Value;
+
+                string positive = p.GetcovidPt();
+
+                if(positive.Equals("YES"))
+                {
+                    Console.WriteLine("Is a Covid Pt"  +positive, "Patient Name  : ", p.Getpatient_Name());
+                }
+                else
+                {
+                    Console.WriteLine("Is Covid Negative");
+                }
+
+            }
+           
+
+        }
+            
+    }
+}
